4f66df1f2c8c5ab1ee301a379fc3f82243b6e0cf
Anthony Badran <anthony.badran@cardinalhealth.com>
2022-06-01 13:21:34 +0300

    Adding bootstrap for data. Closes #14

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.services.PetService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+    @Override
+    public Set<Pet> findAll() {
+        return null;
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return null;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Pet object) {
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.VetService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+    @Override
+    public Set<Vet> findAll() {
+        return null;
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return null;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Vet object) {
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+}

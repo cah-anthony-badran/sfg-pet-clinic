8c99fc0c78b8bc3b40aca9bd4ff509ff8238720b
Anthony Badran <anthony.badran@cardinalhealth.com>
2022-06-01 14:55:03 +0300

    Adding vet list and updated controller. Closes #16

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgpetclinic.services.map;
 
+import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.services.PetService;
 import org.springframework.stereotype.Service;
@@ -10,26 +11,26 @@ import java.util.Set;
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
-        return null;
-    }
-
-    @Override
-    public Pet save(Pet object) {
-        return null;
+        return super.findAll();
     }
 
     @Override
     public Pet findById(Long id) {
-        return null;
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
     }
 
     @Override
     public void delete(Pet object) {
-
+        super.delete(object);
     }
 
     @Override
     public void deleteById(Long id) {
-
+        super.deleteById(id);
     }
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -10,26 +10,26 @@ import java.util.Set;
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
-        return null;
+        return super.findAll();
     }
 
     @Override
     public Vet save(Vet object) {
-        return null;
+        return super.save(object.getId(), object);
     }
 
     @Override
     public Vet findById(Long id) {
-        return null;
+        return super.findById(id);
     }
 
     @Override
     public void delete(Vet object) {
-
+        super.delete(object);
     }
 
     @Override
     public void deleteById(Long id) {
-
+        super.deleteById(id);
     }
 }
